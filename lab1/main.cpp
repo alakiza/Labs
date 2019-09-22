b0d4d024fc3cc729f7bc774cafbe992d6bae7ee4
alakiza <49290743+alakiza@users.noreply.github.com>
2019-09-23 01:22:14 +0300

    Add files via upload

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <math.h>
 
 using namespace std;
@@ -46,8 +47,19 @@ void ShowArray(int* mass, size_t length)
     printf("\n");
 }
 
+void Show2DArray(int** arr, size_t rows, size_t col)
+{
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < col; j++)
+            printf("%5d", arr[i][j]);
+        printf("\n");
+    }
+}
+
 void QSort(int* mass, int left, int right)
 {
+    if (right < left) return;
     int tmp = 0;
     int l = left;
     int r = right;
@@ -82,7 +94,14 @@ int* InitArray()
 
 int** ConvertArrayTo2D(int* input)
 {
+    int** res = new int*[ex2_maslen/2];
+    for(int i = 0; i < ex2_maslen/2; i++) res[i] = new int[2];
 
+    int i = 0;
+    for(int j = 0; j < ex2_maslen/2; j++)
+        for(int k = 0; k < 2; k++) res[j][k] = input[i++];
+
+    return res;
 }
 
 void ex2()
@@ -92,12 +111,89 @@ void ex2()
     ShowArray(input, ex2_maslen);
     QSort(input, 0, ex2_maslen-1);
     ShowArray(input, ex2_maslen);
+    int** out = ConvertArrayTo2D(input);
+
+    Show2DArray(out, ex2_maslen/2, 2);
+
+    for(int i = 0; i < ex2_maslen/2; i++) delete[] out[i];
+    delete []out;
+    delete []input;
+}
+
+char* strcpy(char* destination, char* source)
+{
+    int i = 0;
+    for(i = 0; destination[i] != 0; i++){};
+    int length = i;
+    for(i = 0; source[i] != 0; i++){};
+    length += i + 1;
+
+    char* res = new char[length];
+
+    char* ptrres = res;
+    char* ptrd = destination;
+    char* ptrs = source;
+
+    while(*ptrres++ = *ptrd++);
+    ptrres--;
+    while(*ptrres++ = *ptrs++);
+
+    return res;
+}
+
+/*char* strcpy(char* destination, char* source)
+{
+    char* ptrd = destination;
+    char* ptrsrc = source;
+
+    while(*ptrd) ptrd++;
+    while(*ptrd++ = *ptrsrc++);
+    return destination;
+}*/
+
+void ex3()
+{
+    char* str1 = new char[80];
+    char* str2 = new char[80];
+
+    cin.getline(str1, 80);
+    cin.getline(str2, 80);
+
+    char* str3 = strcpy(str1, str2);
+
+    printf("%s\n", str1);
+    printf("%s\n", str2);
+    printf("%s\n", str3);
+
+    delete[] str1;
+    delete[] str2;
+    delete[] str3;
 }
 
 int main()
 {
-    int num = 0;
-    ex2();
+    char num = 0;
+    do
+    {
+        num = 0;
+        cout << "Введите номер задания(e для выхода)\n";
+        cin >> num;
+        switch (num) {
+        case '1':
+            ex1();
+            break;
+        case '2':
+            ex2();
+            break;
+        case '3':
+            ex3();
+            break;
+        default:
+            cout << "Введите 1-3\n";
+            break;
+        }
+    }
+    while(num != 'e');
     //cout << "Select exercise\n" << endl;
     //cin >> num;
     //cout << num  << endl;
